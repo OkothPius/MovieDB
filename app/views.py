847b9ebe84fae78b2f6da653ac2e5327734636b6
Okoth Pius <orukopius8@gmail.com>
2022-01-29 00:06:14 +0300

    [feat] Update app

--- app/views.py
+++ app/views.py
@@ -1,12 +1,15 @@
 from flask import render_template
-
 from app import app
 
-
-#
+# Views
 @app.route('/')
 def index():
     '''
     view root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title=title)
+
+@app.route('/movies/<int:movie_id>')
+def movie(movie_id: int):
+    return render_template('movie.html', id=movie_id)
