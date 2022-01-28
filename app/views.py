35f065df297147e82bb42359fb62d3aeff336dfd
Okoth Pius <orukopius8@gmail.com>
2022-01-28 23:33:00 +0300

    [feat] update

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from flask import render_template
+
+from app import app
+
+
+#
+@app.route('/')
+def index():
+    '''
+    view root page function that returns the index page and its data
+    '''
+    return render_template('index.html')
