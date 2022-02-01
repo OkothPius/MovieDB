ce76668aa61987fb3ed8d7946b71397ac069bc64
Okoth Pius <orukopius8@gmail.com>
2022-02-01 18:52:32 +0300

    [feat] Update

--- manage.py
+++ manage.py
@@ -1,12 +1,15 @@
 from app import create_app
+# from flask.cli
+# import FlaskGroup
 
 from flask_script import Manager, Server
 
 # Creating an app instance
 app = create_app('development')
 
-
 manager = Manager(app)
+# cli = FlaskGroup(app)
+
 manager.add_command('server', Server)
 
 if __name__ == '__main__':
