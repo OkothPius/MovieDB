3265573ac8ef48eae9466a6798b6f510be20450c
Okoth Pius <orukopius8@gmail.com>
2022-02-01 21:43:22 +0300

    [feat] Update manage.py

--- manage.py
+++ manage.py
@@ -11,6 +11,12 @@ manager = Manager(app)
 # cli = FlaskGroup(app)
 
 manager.add_command('server', Server)
+@manager.command
+def test():
+    """ Run the unit test. """
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__ == '__main__':
     manager.run()
