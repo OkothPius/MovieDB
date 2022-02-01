8f0bf1eca5a712d7c1996fbd6042b8e133660aad
Okoth Pius <orukopius8@gmail.com>
2022-02-01 18:52:14 +0300

    [feat] Update

--- config.py
+++ config.py
@@ -4,30 +4,21 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    '''
-    General configuration parent class
-    '''
     MOVIE_API_BASE_URL= 'https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.getenv('MOVIE_API_KEY')
     SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 class ProdConfig(Config):
-    '''
-    Production  configuration child class
 
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
     pass
 
 
 class DevConfig(Config):
-    '''
-    Development  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
 
     DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
