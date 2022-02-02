0c57607cd651c8b6cd34dcbddec6af8f0dfa15e3
Okoth Pius <orukopius8@gmail.com>
2022-02-02 09:31:56 +0300

    [feat] Update db

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config:
     MOVIE_API_BASE_URL= 'https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.getenv('MOVIE_API_KEY')
     SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://watchlist:pyglass@localhost/watchlist'
 
 
 class ProdConfig(Config):
