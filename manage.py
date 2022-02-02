6f740e897022e5313602a67989ea316751f695a6
Okoth Pius <orukopius8@gmail.com>
2022-02-02 13:08:58 +0300

    [feat] Update database

--- manage.py
+++ manage.py
@@ -1,17 +1,22 @@
 from app import create_app, db
-from app.models import User
+from app.models import User, Role
 # from flask.cli
 # import FlaskGroup
 
 from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
+
 
 # Creating an app instance
 app = create_app('development')
 
 manager = Manager(app)
+migrate = Migrate(app,db)
 # cli = FlaskGroup(app)
 
+manager.add_command('db', MigrateCommand)
 manager.add_command('server', Server)
+
 
 @manager.command
 def test():
@@ -22,7 +27,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, User=User)
+    return dict(app=app, db=db, User=User, Role=Role)
 
 if __name__ == '__main__':
     manager.run()
