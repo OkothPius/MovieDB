08b775d60b0aaa10345fa8a2eec4c372a2fb9756
Okoth Pius <orukopius8@gmail.com>
2022-02-02 09:31:24 +0300

    [feat] Update db

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,23 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 from config import config_options
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 def create_app(config_name):
 
     app = Flask(__name__)
+
+
 
     # Setting up app configuration
     app.config.from_object(config_options[config_name])
 
     # Initializing flask extensions
     bootstrap.init_app(app)
+    db.init_app(app)
 
     # Registering Blueprint
     from .main import main as main_blueprint
