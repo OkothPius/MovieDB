fb1de8aeec568252205c0633eb53e40c4a93955e
Okoth Pius <orukopius8@gmail.com>
2022-02-02 18:28:33 +0300

    [test] Update tests

--- tests/test_review.py
+++ tests/test_review.py
@@ -1,29 +1,31 @@
 import unittest
-from app.models import Review
+from app.models import Review, User
+from app import db
 
 class TestReview(unittest.TestCase):
 
     def setUp(self):
-        self.new_review = Review(12345,'Review for movies',"https://image.tmdb.org/t/p/w500/jdjdjdjn",'This movie is the best thing since sliced bread')
+        self.user_James = User(username = 'James',password = 'potato', email = 'james@ms.com')
+        self.new_review = Review(
+                        movie_id=12345,
+                        movie_title='Review for movies',
+                        image_path="https://image.tmdb.org/t/p/w500/jdjdjdjn",
+                        movie_review='This movie is Great',
+                        user = self.user_James
+                        )
 
 
     def tearDown(self):
-        Review.clear_reviews()
-
-    def test_instance(self):
-        self.assertTrue(isinstance(self.new_review,Review))
+        Review.query.delete()
+        User.query.delete()
 
 
     def test_check_instance_variables(self):
         self.assertEquals(self.new_review.movie_id,12345)
-        self.assertEquals(self.new_review.title,'Review for movies')
-        self.assertEquals(self.new_review.imageurl,"https://image.tmdb.org/t/p/w500/jdjdjdjn")
-        self.assertEquals(self.new_review.review,'This movie is the best thing since sliced bread')
-
-
-    def test_save_review(self):
-        self.new_review.save_review()
-        self.assertTrue(len(Review.all_reviews)>0)
+        self.assertEquals(self.new_review.movie_title,'Review for movies')
+        self.assertEquals(self.new_review.image_path,"https://image.tmdb.org/t/p/w500/jdjdjdjn")
+        self.assertEquals(self.new_review.movie_review,'This movie is Great')
+        self.assertEquals(self.new_review.user,self.user_James)
 
 
     def test_get_review_by_id(self):
